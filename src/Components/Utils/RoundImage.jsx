37f8a9e9333614fe4b3e91e540ee1fa7a2fd736e
ben.pipkin <benpipkin.il@gmail.com>
2024-06-26 22:46:11 +0300

    navbar styling

--- src/Components/Utils/RoundImage.jsx
+++ src/Components/Utils/RoundImage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const RoundImage = ({ src }) => {
-  return <img src={src} alt="" class="rounded-full shadow-lg w-12 h-12"></img>;
+  return (
+    <img src={src} alt="" class="rounded-full shadow-lg w-12 h-12 m-1 "></img>
+  );
 };
 
 export default RoundImage;
